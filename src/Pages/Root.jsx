27fbc4c0b15c62bd0bf8d2fec650eddce74c88c7
nafisfaiyaz-012 <contact.nafisfaiyaz@gmail.com>
2025-10-10 20:39:33 +0600

    final push

--- src/Pages/Root.jsx
+++ src/Pages/Root.jsx
@@ -1,24 +1,12 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
-import { Outlet, useNavigation } from "react-router";
-import LoadingSpinner from "./LoadingSpinner";
+import { Outlet } from "react-router";
 
 const Root = () => {
-  const navigation = useNavigation();
-  const isNavigating = navigation.state;
-  // console.log(isNavigating);
-
   return (
     <div className="flex flex-col min-h-screen w-11/12 mx-auto">
       <Navbar></Navbar>
-      {isNavigating === "loading" && (
-        <>
-          <div className="fixed inset-0 z-50 flex items-center justify-center bg-white/70">
-            <LoadingSpinner />
-          </div>
-        </>
-      )}
       <div className="flex-1">
         <Outlet></Outlet>
       </div>
